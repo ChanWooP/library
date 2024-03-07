a71004dc9cd9d95a703f343f22e2489c184ef809
ChanWooP <cksdn4002@gmail.com>
2024-03-07 21:44:01 +0900

    공통 Header 추가

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,3 +1,19 @@
 function home() {
     location.href = '/';
 }
+
+function login() {
+    location.href = '/login';
+}
+
+function logout() {
+    location.href = '/logout';
+}
+
+function myPage() {
+    location.href = '/user/mypage';
+}
+
+function join() {
+    location.href = '/user/join';
+}
