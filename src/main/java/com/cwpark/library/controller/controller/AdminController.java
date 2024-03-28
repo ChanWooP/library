e8b8f95a34c90a5d0876a092312be80a130d6a16
ChanWooP <cksdn4002@gmail.com>
2024-03-28 17:08:24 +0900

    카테고리 화면 추가

--- src/main/java/com/cwpark/library/controller/controller/AdminController.java
+++ src/main/java/com/cwpark/library/controller/controller/AdminController.java
@@ -1,15 +1,24 @@
 package com.cwpark.library.controller.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
     @GetMapping("/main")
-    public String main() {
+    public String mains() {
         return "/admin/main";
+    }
+
+    @GetMapping("/book/category")
+    public String category() {
+        return "/admin/book/category";
     }
 }
