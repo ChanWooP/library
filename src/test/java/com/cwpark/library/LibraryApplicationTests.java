11e9c09f8cd0bee6e7f2a93fc680102a9d16f6bf
ChanWooP <cksdn4002@gmail.com>
2024-05-01 16:36:45 +0900

    메인 테스트 수정

--- src/test/java/com/cwpark/library/LibraryApplicationTests.java
+++ src/test/java/com/cwpark/library/LibraryApplicationTests.java
@@ -3,7 +3,7 @@ package com.cwpark.library;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class LibraryApplicationTests {
 
 	@Test
